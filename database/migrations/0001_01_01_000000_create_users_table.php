edc0c8a9f6429867fd035f77442005b21ae93177
Yatzari <2130039@upv.edu.mx>
2024-12-02 08:03:52 -0600

    backend

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         if(!Schema::hasTable('users')) {
             Schema::create('users', function (Blueprint $table) {
                 $table->id();
+                $table->string('username');
                 $table->string('nombre');
                 $table->string('apellido');
                 $table->string('usuario');
---
+++ database/migrations/2024_12_02_135051_create_ventas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->string('rutaImagen');
+            $table->string('nombre');
+            $table->float('precio');
+            $table->integer('categoria');
+            $table->string('descripcion')->nullable(true);
+            $table->string('unidadesDisponibles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ventas');
+    }
+};
